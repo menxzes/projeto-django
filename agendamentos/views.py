fe0f99c6ce94e1c4238d90da37ae02f87adf0447
Guilherme Menezes <159064786+menxzes@users.noreply.github.com>
2025-05-15 13:27:09 -0300

    fix: correção de erros nos agendamentos

--- agendamentos/views.py
+++ agendamentos/views.py
@@ -1,18 +1,15 @@
-from django.shortcuts import render
-from .models import Servico
+from django.shortcuts import render, get_object_or_404
+from .models import Servico, Profissional
 
 def lista_servicos(request):
     servicos = Servico.objects.filter(ativo=True)
     return render(request, 'agendamentos/lista_servicos.html', {'servicos': servicos})
 
 def horarios_disponiveis(request, servico_id):
-    servico = Servico.objects.get(id=servico_id)
+    servico = get_object_or_404(Servico, id=servico_id)  # Corrigido aqui
     profissionais = Profissional.objects.filter(servico=servico)
     
-    return render(request,
-        'agendamentos/horarios_disponiveis.html',
-        {
-            'servico': servico,
-            'profissionais': profissionais
-        }
-    )
+    return render(request, 'agendamentos/horarios_disponiveis.html', {
+        'servico': servico,
+        'profissionais': profissionais
+    })
